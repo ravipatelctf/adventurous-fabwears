c2c80582f00e0ce78193c825539d2ac15a81c560
ravipatelctf <ravipatelctf@gmail.com>
2025-12-24 15:26:58 +0530

    remove google analytics code

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -58,30 +58,11 @@ export default function Layout({
 }: {
   children: React.ReactNode
 }) {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
   const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        {GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="ga-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `}
-            </Script>
-          </>
-        )}
         {/* Google Tag Manager */}
         <Script
           id="gtm-script"
