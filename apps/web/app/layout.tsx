e9f042f4694105f1920dc36a6bb61db53a9c39a3
ravipatelctf <ravipatelctf@gmail.com>
2025-12-24 14:17:42 +0530

    add google tag manager

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -58,7 +58,8 @@ export default function Layout({
 }: {
   children: React.ReactNode
 }) {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID
+  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -81,12 +82,38 @@ export default function Layout({
             </Script>
           </>
         )}
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
       </head>
 
       <body className="font-sans antialiased">
         <Providers>
           <Suspense fallback={null}>
             <GAPageView />
+              {/* Google Tag Manager (noscript) */}
+              <noscript>
+                <iframe
+                  src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                  height="0"
+                  width="0"
+                  style={{ display: "none", visibility: "hidden" }}
+                />
+              </noscript>
           </Suspense>
           {children}
         </Providers>
